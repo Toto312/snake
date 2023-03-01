6dad8386e33db4b43b71daa85fb4902dd54f7887
Toto312 <tobiaspfaffen@gmail.com>
2023-03-01 16:29:52 -0300

    arrange things

--- src/game.py
+++ src/game.py
@@ -5,11 +5,23 @@ import copy
 import random
 from font import Font
 import math
+from grid import real_position
 
 class Game:
     pygame.init()
 
     def __init__(self,width=None, height=None):
+
+        self.snake_clock = pygame.time.get_ticks()
+        self.clock = pygame.time.Clock()
+        self.FPS = 60        
+        self.is_paused = True
+        self.is_dead = False
+        self.score = 0
+        
+        """
+            Things than doesnt need to be in this class
+        """
         if(width):
             self.width = width
         if(height):
@@ -20,15 +32,12 @@ class Game:
             self.height = 760
 
         self.screen = pygame.display.set_mode((self.width, self.height))
-        self.score = 0
         self.block_size = 38
         
         self.game_over = Font("Resources/PixeloidSans.ttf", "Game over. Press \"r\" to play again", False,35)
         self.resume = Font("Resources/PixeloidSans.ttf", "Press \"p\" to resume the game", False)
         self.score_title = Font("Resources/PixeloidSans.ttf", "Score: {0}".format(self.score), False,35)
-        self.is_paused = True
-        self.is_dead = False
-
+       
         self.snake = pygame.sprite.Group()
         self.apple = pygame.sprite.Group()
 
@@ -39,10 +48,10 @@ class Game:
         self.color_apple=(224,113,113)
 
         self.snake_position = [-1,0]
-        self.snake_clock = pygame.time.get_ticks()
-
-        self.clock = pygame.time.Clock()
-        self.FPS = 60
+        
+        """
+            ---------------------------------------------
+        """
 
     def snake_update(self,dt):
         if(self.is_paused or self.is_dead):
@@ -59,7 +68,7 @@ class Game:
 
         last_pos = head.rect[0:2]
 
-        head.change_values(self.real_position(self.snake_position),True)
+        head.change_values(real_position(self.snake_position),True)
             
         for i in range(len(self.snake.sprites())-1):
             i+=1
@@ -81,7 +90,7 @@ class Game:
                 break
             rand = [random.randint(0,self.x_max),random.randint(0,self.y_max)]
             
-            if(not self.real_position(rand) in not_posibilites):
+            if(not real_position(rand) in not_posibilites):
                 position=rand
                 break
             i+=1
@@ -95,7 +104,7 @@ class Game:
         for i in not_posibilites:
             print(i[0]/38,i[1]/38)"""
         for i in self.apple:
-            i.change_values(self.real_position(position))
+            i.change_values(real_position(position))
 
     def init_apple(self):
         not_posibilites = []
@@ -107,12 +116,12 @@ class Game:
 
         for i in range(20):
             rand = [random.randint(0,self.x_max),random.randint(0,self.y_max)]
-            if(not [self.real_position(rand)] in not_posibilites):
+            if(not [real_position(rand)] in not_posibilites):
                 position = rand
                 break
         
         apple = sprite.Sprite(self.color_apple, self.block_size)
-        apple.change_values(self.real_position(position))
+        apple.change_values(real_position(position))
         self.apple.add(apple)
 
     def check_collision(self):
@@ -155,11 +164,11 @@ class Game:
             self.snake.draw(self.screen)
 
             if(self.is_paused and not self.is_dead):
-                self.resume.render(self.screen, self.real_position([self.x_max/2*1.02,(self.y_max/2)*1.75]))
+                self.resume.render(self.screen, real_position([self.x_max/2*1.02,(self.y_max/2)*1.75]))
             elif(self.is_dead):
-                self.game_over.render(self.screen, self.real_position([self.x_max/2,(self.y_max/2)*1.75]))
+                self.game_over.render(self.screen, real_position([self.x_max/2,(self.y_max/2)*1.75]))
             
-            self.score_title.render(self.screen,self.real_position([self.x_max*0.8,1]))
+            self.score_title.render(self.screen,real_position([self.x_max*0.8,1]))
 
             pygame.display.update()
 
@@ -202,7 +211,7 @@ class Game:
 
     def createBlock(self,coordenade,color):
         sprites = sprite.Sprite(color,self.block_size)
-        sprites.change_values(self.real_position(coordenade))
+        sprites.change_values(real_position(coordenade))
         self.snake.add(sprites)
 
 if(__name__=="__main__"):
