53ca0d0057051c470c48f698cccc5f87f1c26617
Toto312 <tobiaspfaffen@gmail.com>
2023-03-04 23:51:27 -0300

    create an ui manager class to work with the fonts independently

---
+++ src/ui_manager.py
@@ -0,0 +1,25 @@
+import pygame
+
+class UIManager:
+    def __init__(self,state):
+        #fonts in UIManager, its an 2d array
+        self.fonts = []
+        self.screen = pygame.display.get_surface()
+        self.state = state
+
+    def add_font(self,font,what_state_activate):
+        self.fonts.append([font,what_state_activate])
+
+    def change_name(self,font,name):
+        for i in self.fonts:
+            if(i[0].text == font.text):
+                i[0].text = name[:]
+
+            
+    def show_fonts(self):
+        for i in self.fonts:
+            if(i[1] == "always"):
+                i[0].render(self.screen)
+
+            elif(i[1].ret_values() == self.state.ret_values()):
+                i[0].render(self.screen)
