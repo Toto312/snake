53701fed9b44b07f0859fbd5b7a328bea9ebe9b9
Toto312 <tobiaspfaffen@gmail.com>
2023-03-05 17:19:56 -0300

    delete unnecessary class

--- src/grid.py
+++ src/grid.py
@@ -3,15 +3,4 @@ import pygame
 #also make a grid class to handle position
 def real_position(coord):
     return [coord[0]*38,coord[1]*38]
-
-class Vector:
-    def __init__(self,values):
-        self.x = values[0]
-        self.y = values[1]
-    def __add__(self,vector):
-        self.x += vector.x
-        self.y = vector.y
-
-
-if(__name__=="__main__"):
-    pass
+    
